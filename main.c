79e6eddd7a7f0e4b726c7c639643bf831bcb8875
LuigiRuotolo00 <luigiruotolo7@outlook.it>
2025-09-14 16:43:27 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,11 +1,30 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+	#include <conio.h>
+	#include <windows.h>
+#endif
 
 #include "menu.h"
-#include "input.h"
+#ifdef __linux__
+	#include "input.h"
+#endif
 #include "game.h"
 #include "game_rules.h"
+
+#ifdef __linux__
+	#ifndef ENTER
+		#define ENTER 10
+	#endif
+#else
+	#ifndef ENTER
+		#define ENTER 13
+	#endif
+#endif
 
 int main(void)
 {
@@ -16,20 +35,41 @@ int main(void)
 	char key = 0;
 	uint8_t pos;
 
-	signal(SIGINT, interrupt_handler);
+	char clear[5];
+
+	memset(clear, 0, sizeof(clear));
+
+	#ifdef __linux__
+			clear[0] = 'c';
+			clear[1] = 'l';
+			clear[2] = 'e';
+			clear[3] = 'a';
+			clear[4] = 'r';
+ 		#elif defined(_WIN32)
+			clear[0] = 'c';
+			clear[1] = 'l';
+			clear[2] = 's';
+ 		#endif
+ 		
+ 	#ifdef __linux__
+		signal(SIGINT, interrupt_handler);
+	#endif
 	printf("\e[?25l");
 
 	while (t_session)
 	{
-		system("clear");
+		system(clear);
 		display_main_menu();
 
 		while (t_menu)
 		{
 			flag_display_menu = 0;
-			//key = 0;
 
-			key = kbhit();
+			#ifdef __linux__
+				key = kbhit();
+			#elif defined(_WIN32)
+				key = getch();
+			#endif
 
 			flag_display_menu = 1;
 
@@ -41,14 +81,14 @@ int main(void)
 				case 's':
 					change_pointer_position(0);
 					break;
-				case 10:
+				case ENTER:
 					pos = get_pointer_position();
 					t_menu = 0;
 					flag_display_menu = 0;
 					if (pos == 0)
 					{
 						t_game = 1;
-						system("clear");
+						system(clear);
 						game_init();
 					}else if (pos == 1)
 					{
@@ -63,17 +103,19 @@ int main(void)
 
 			if (flag_display_menu == 1)
 			{
-				system("clear");
+				system(clear);
 				display_main_menu();
 			}
 		}
 
-		//system("clear");
-		//display_grid();
 
 		while (t_game)
 		{
-			key = kbhit();
+			#ifdef __linux__
+				key = kbhit();
+			#elif defined(_WIN32)
+				key = getch();
+			#endif
 
 			flag_display_game = 1;
 
@@ -84,19 +126,31 @@ int main(void)
 				t_game = 0;
 				t_menu = 1;
 				printf("%s WIN\n", get_player_name(1));
-				sleep(2);
+				#ifdef __linux__
+					sleep(2);
+				#elif defined(_WIN32)
+					Sleep(2000);
+				#endif
 			}else if (check_rules(2))
 			{
 				t_game = 0;
 				t_menu = 1;
 				printf("%s WIN\n", get_player_name(1));
-				sleep(2);
+				#ifdef __linux__
+					sleep(2);
+				#elif defined(_WIN32)
+					Sleep(2000);
+				#endif
 			}else if (check_no_win())
 			{
 				t_game = 0;
 				t_menu = 1;
 				printf("NOONE WIN\n");
-				sleep(2);
+				#ifdef __linux__
+					sleep(2);
+				#elif defined(_WIN32)
+					Sleep(2000);
+				#endif
 			}
 		}
 	}
